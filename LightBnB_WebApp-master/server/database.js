94fc6966b2e4e3d4ffe51accefd597afc6829109
Manuel Zuniga <zuniga@ualberta.ca>
2022-03-10 18:17:25 -0700

    updated functions for getuserwithemail, getuserwithid and adduser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,14 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  let userEmail = email.toLowerCase();
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.email = $1
+  ;`, [userEmail])
+  .then(res =>  res.rows[0]);
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -37,7 +34,10 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.id = $1;`, [id])
+  .then(res =>  res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +48,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  ;`, [user.name, user.email, user.password]) // The returning * is handy when you need the auto generated id of an object you've just added to the database
+  .then(res =>  res.rows[0]);
 }
 exports.addUser = addUser;
 
